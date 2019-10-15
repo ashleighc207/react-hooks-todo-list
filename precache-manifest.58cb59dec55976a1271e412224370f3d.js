7b917b6574c6bc94ba2da8127bd09ed54e637dad
Ashleigh Christopher <ashleighc207@gmail.com>
2019-10-15 13:11:31 -0400

    Updates

---
+++ precache-manifest.58cb59dec55976a1271e412224370f3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b28565956fb05488c060ea5fcfa6e2",
+    "url": "/react-hooks-todo-list/index.html"
+  },
+  {
+    "revision": "db387c8652f312db9471",
+    "url": "/react-hooks-todo-list/static/css/main.7ed6dd43.chunk.css"
+  },
+  {
+    "revision": "629f6870509dd6b97069",
+    "url": "/react-hooks-todo-list/static/js/2.1a2fa593.chunk.js"
+  },
+  {
+    "revision": "db387c8652f312db9471",
+    "url": "/react-hooks-todo-list/static/js/main.b1c0934e.chunk.js"
+  },
+  {
+    "revision": "376ee268ef3a6f9dffbc",
+    "url": "/react-hooks-todo-list/static/js/runtime-main.7be63587.js"
+  }
+]);
